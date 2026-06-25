017d35cb51778b5f085d9e727b12362dc4781fac
Cyberman884 <asandabasie@gmail.com>
2026-06-25 23:28:09 +0200

    Add files via upload

--- supabase_client.py
+++ supabase_client.py
@@ -4,10 +4,4 @@ from config import SUPABASE_URL, SUPABASE_KEY
 supabase = create_client(
     SUPABASE_URL,
     SUPABASE_KEY
-)from supabase import create_client
-from config import SUPABASE_URL, SUPABASE_KEY
-
-supabase = create_client(
-    SUPABASE_URL,
-    SUPABASE_KEY
 )
